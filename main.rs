7c5a97e38a35f1488a4842da533813a1a13f3254
Alexandre Desmarais <alexandre-2.desmarais@outlook.com>
2025-01-02 15:45:01 -0500

    Initial commit

---
+++ main.rs
@@ -0,0 +1,16 @@
+fn main(){
+
+    // Image
+    let image_width : u32 = 256;
+    let image_height : u32 = 256;
+
+    // Render
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height{
+        for i in 0..image_width{
+
+        }
+    }
+
+}
