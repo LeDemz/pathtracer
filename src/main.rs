401963484188e3ec60352a778570e3fa48ad4fc0
Alexandre Desmarais <alexandre-2.desmarais@outlook.com>
2025-01-02 17:37:37 -0500

    Made correct output

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,16 @@ fn main(){
 
     for j in 0..image_height{
         for i in 0..image_width{
+        let r : f64 = (i as f64) / (image_width as f64 -1.0);
+        let g : f64 = (j as f64) / (image_height as f64 -1.0);
+        let b : f64 = 0.0;
 
+
+        let ir : i32 = (255.999 *r).floor() as i32; 
+        let ig : i32 = (255.999 *g).floor() as i32;
+        let ib : i32 = (255.999 *b).floor() as i32;
+
+        println!("{} {} {}", ir,ig,ib);
         }
     }
 
