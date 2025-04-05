9a5f6f21b5e5045c79abe98212654c38945f63c3
Alexandre Desmarais <alexandre-2.desmarais@outlook.com>
2025-04-05 14:52:53 -0400

    Not working camera

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use pathtracer::Lambertian;
 use pathtracer::Metal;
 use pathtracer::Point3;
 use pathtracer::Sphere;
+use pathtracer::Vec3;
 use pathtracer::PI;
 
 use std::rc::Rc;
@@ -13,18 +14,37 @@ use std::rc::Rc;
 fn main() {
     let R = f64::cos(PI / 4.0);
 
-    let material_left = Rc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
-    let material_right = Rc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.50));
+    let material_bubble = Rc::new(Dielectric::new(1.00 / 1.50));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     let mut world = HittableList::new();
+
     world.add(Rc::new(Sphere::new(
-        Point3::new(-R, 0.0, -1.0),
-        R,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        material_center,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
         material_left,
     )));
     world.add(Rc::new(Sphere::new(
-        Point3::new(R, 0.0, -1.0),
-        R,
+        Point3::new(-1.0, 0.0, -1.0),
+        0.4,
+        material_bubble,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         material_right,
     )));
 
@@ -35,6 +55,11 @@ fn main() {
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
     cam.fvov = 90.0;
+
+    cam.lookfrom = Point3::new(-2.0,2.0,1.0);
+    cam.lookat   = Point3::new(0.0,0.0,-1.0);
+    cam.vup      = Vec3::new(0.0,1.0,0.0);
+
 
     cam.render(&world);
 }
